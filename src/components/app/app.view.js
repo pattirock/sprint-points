cd82b4283690e167a7ccc8f29f9b52af5ddecc33
pattirock <pmateosromero@gmail.com>
2020-07-25 11:28:57 +0200

    Basic project structure

---
+++ src/components/app/app.view.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './app.module.css';
+
+const App = () => (
+  <div className={styles._container}>I'm an App!</div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  devServer: {
+    stats: 'errors-only',
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    overlay: true,
+    clientLogLevel: 'trace',
+    onListening: (server) => {
+      const { port } = server.listeningApp.address();
+      console.log('Starting the development server on port: ', port, '\n');
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              sourceMap: true,
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
